f227d42a8919d71ac5f81bee0c5fcbc7f4d49f21
Erikomoreira <erik.oliveira.moreira@hotmail.com>
2021-04-05 18:21:18 -0300

    Par e Impar Job

---
+++ parImparJob/src/test/java/br/com/erik/parImparJob/ParImparJobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erik.parImparJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParImparJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
